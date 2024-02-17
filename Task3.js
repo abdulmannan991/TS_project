9fa20169b65f4d2816d8bacd00828f501f34c3a6
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-17 16:12:57 +0500

    Task 3

---
+++ Task3.js
@@ -0,0 +1,1 @@
+console.log("Quaid - e - Azam Muhammad Ali Jinnah once said \"Work work and work and we are bound to success\" ");
---
+++ Task3.ts
@@ -0,0 +1,1 @@
+console.log("Quaid - e - Azam Muhammad Ali Jinnah said once said : _ _ Work work and work and we are bound to success");
