1be5c2fe0f48c5c6a7f123284c63fe1860d265ea
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 11:16:42 +0500

    Task 15

---
+++ task15.js
@@ -0,0 +1,19 @@
+var List = [
+    "M.Shayan",
+    "Faraz",
+    "Siraj",
+];
+console.log("Dear " + List[0] + ", I would be happy if you could come to dinner!");
+console.log("Dear " + List[1] + ", I would be delighted if you could join me for dinner!");
+console.log("Dear " + List[2] + ", It would be a pleasure to have you at dinner!");
+console.log("\nchange in the guest list\n");
+var oldPerson = "Faraz";
+var newPerson = "Shahmir";
+var indexOfOldPerson = List.indexOf(oldPerson);
+if (indexOfOldPerson !== -1) {
+    console.log(oldPerson + " can't make it to dinner.");
+    List[indexOfOldPerson] = newPerson;
+}
+console.log("Dear " + List[0] + ", I would be happy if you could come to dinner!");
+console.log("Dear " + List[1] + ", I would be delighted if you could join me for dinner!");
+console.log("Dear " + List[2] + ", It would be a pleasure to have you at dinner!");
