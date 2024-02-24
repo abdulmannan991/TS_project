8729839ab65ae14bc94c6256ba200484f30b9ad0
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 10:44:19 +0500

    Task 15

---
+++ task15.js
@@ -0,0 +1,8 @@
+var List = [
+    "M.Shayan",
+    "Faraz",
+    "Siraj",
+];
+console.log("Dear " + List[0] + ", I would be happy if you could come to dinner!");
+console.log("Dear " + List[1].replace("Faraz", "Shahmir") + ", I would be delighted if you could join me for dinner!");
+console.log("Dear " + List[2] + ", It would be a pleasure to have you at dinner!");
