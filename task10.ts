276cc0f2cfb4a1071ddb4f24f032e8da9d1a1cb9
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 09:33:26 +0500

    Task 10

---
+++ task10.ts
@@ -0,0 +1,20 @@
+var num1 = 5;
+var num2 = 6;
+var add = num1 + num2;
+
+console.log(add);
+
+// This program performs a simple mathematical calculation. It adds two given numbers stored in variables.
+console.log("____________________________________________________");
+
+//         15 if we wrote 15 here then the reminder is 1;
+var num1 = 12; 
+var num2 = 2;
+var modulos = num1 % num2;
+
+console.log(modulos);
+
+// In this program we find if the number is divisible by 2 or not if it divisible by 2 then it will give 
+// the output in zero otherwise the output it will show it will be a reamianing number!!
+
+                                    //   Thank You!!
