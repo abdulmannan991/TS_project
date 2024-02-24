f847850a286d5b670bf2821332e1ce15604d364d
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 09:05:10 +0500

    Task 7

---
+++ task7.js
@@ -0,0 +1,6 @@
+var add = 4 + 4;
+console.log(add);
+var sub = 10 - 2;
+console.log(sub);
+var multiplication = 2 * 4;
+console.log(multiplication);
---
+++ task7.ts
@@ -0,0 +1,10 @@
+var add = 4 + 4;
+
+console.log(add);
+
+var sub = 10 - 2;
+
+console.log(sub);
+
+var multiplication = 2 * 4;
+console.log(multiplication);
