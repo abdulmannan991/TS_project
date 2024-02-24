5aad9adc56a1ef2e2245e879b4302c05e5a1595c
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 09:46:16 +0500

    Task 12

---
+++ task12.ts
@@ -0,0 +1,10 @@
+let List = ["Abdul Mannan" , "M.Shayan" , "Faraz" , "Siraj" ];
+
+console.log("Welcome!",List[0]);
+
+console.log("Welcome!",List[1]);
+
+console.log("Welcome!",List[2]);
+
+console.log("Welcome!",List[3]);
+
