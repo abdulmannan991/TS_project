8e3e39d9d543b89496b1ec6a0739207ad5229262
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 09:40:56 +0500

    Task 11

---
+++ task11.js
@@ -0,0 +1,5 @@
+var List = ["Abdul Mannan", "M.Shayan", "Faraz", "Siraj"];
+console.log(List[0]);
+console.log(List[1]);
+console.log(List[2]);
+console.log(List[3]);
---
+++ task11.ts
@@ -0,0 +1,10 @@
+let List = ["Abdul Mannan" , "M.Shayan" , "Faraz" , "Siraj" ];
+
+console.log(List[0]);
+
+console.log(List[1]);
+
+console.log(List[2]);
+
+console.log(List[3]);
+
