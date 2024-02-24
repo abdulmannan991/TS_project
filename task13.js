87f35cca2e2df9d5631fe66a82cb9232789c6aef
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 10:04:38 +0500

    Task 13

---
+++ task13.js
@@ -0,0 +1,4 @@
+var List = ["I would like to own a Honda motorcycle.", "I would like to own a Sonata Car", "I would like to own my private airplane"];
+console.log(List[0]);
+console.log(List[1]);
+console.log(List[2]);
