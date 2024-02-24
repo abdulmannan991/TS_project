4ea671ac16ed3f4736248255ca77259bbee625fd
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-02-24 10:12:51 +0500

    Task 14

---
+++ task14.js
@@ -0,0 +1,8 @@
+var List = [
+    "M.Shayan",
+    "Faraz",
+    "Siraj",
+];
+console.log(List[0], ":I would be happy if you will come to dinner!!");
+console.log(List[1], ":I would be delighted if you could join me for dinner!!");
+console.log(List[2], ":It would be a pleasure to have you at dinner!");
---
+++ task14.ts
@@ -0,0 +1,10 @@
+var List = [
+    "M.Shayan",
+    "Faraz",
+    "Siraj",
+  ];
+  
+  console.log(List[0],":I would be happy if you will come to dinner!!");
+  console.log(List[1],":I would be delighted if you could join me for dinner!!");
+  console.log(List[2],":It would be a pleasure to have you at dinner!");
+  
