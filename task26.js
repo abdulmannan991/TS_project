897f0d03b1366544de220c381cccf479e7541033
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:23:16 +0500

    task26

---
+++ task26.js
@@ -0,0 +1,14 @@
+var alien_color = "Green";
+if (alien_color === "Green") {
+    console.log("Congrats You get 5 points for shooting the alien");
+}
+else {
+    console.log("Congrats! You get 10 points");
+}
+var alien_color2 = "yellow";
+if (alien_color2 === "Green") {
+    console.log("Congrats! You get 5 points for shooting the alien");
+}
+else {
+    console.log("Congrats! You get 10 points");
+}
---
+++ task26.ts
@@ -0,0 +1,19 @@
+let alien_color = "Green";
+
+if (alien_color === "Green") {
+    console.log("Congrats You get 5 points for shooting the alien");
+}
+else{
+console.log("Congrats! You get 10 points");
+
+}
+
+let alien_color2 = "yellow";
+
+if (alien_color2 === "Green") {
+    console.log("Congrats! You get 5 points for shooting the alien");
+}
+else{
+console.log("Congrats! You get 10 points");
+
+}
