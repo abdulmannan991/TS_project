8610f1f9ac16ed4c2d5a2628f70ff847a6ae1390
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:23:45 +0500

    task27

---
+++ task27.js
@@ -0,0 +1,13 @@
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("Congrats You get 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("Congrats You get 10 points");
+}
+else if (alien_color.toLowerCase() === "red") {
+    console.log("Congrats! You get 15 points.");
+}
+else {
+    console.log("The color is invalid!!");
+}
