425ee70c9638986e4f2e0f6d541dfcb63523e141
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:21:43 +0500

    task23

---
+++ task23.ts
@@ -0,0 +1,46 @@
+
+
+console.log("%%%%%True Statements%%%%%");
+
+let car1 = "Sonata";
+
+console.log(car1=="Sonata");
+
+let car2 = "Sportage";
+
+console.log(car2=="Sportage");
+
+let car3 = "Tesla";
+
+console.log(car3=="Tesla");
+
+let car4 = "Lamborghini";
+
+console.log(car4=="Lamborghini");
+
+let car5 = "Civic";
+
+console.log(car5=="Civic");
+
+console.log("%%%%%False Statements%%%%%");
+
+
+let car6 = "Sonat";
+
+console.log(car6=="Sonata");
+
+let car7 = "Sportag";
+
+console.log(car7=="Sportage");
+
+let car8 = "Tesl";
+
+console.log(car8=="Tesla");
+
+let car9 = "Lamborghin";
+
+console.log(car9=="Lamborghini");
+
+let car10 = "Civi";
+
+console.log(car10=="Civic");
