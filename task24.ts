0f64093d8a0a8ef81bb95d2de42ddabdcb72f5d9
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:22:12 +0500

    task24

---
+++ task24.ts
@@ -0,0 +1,51 @@
+let name1 = "apple";
+let name2 = "apple";
+
+let result1 = name1 == name2 ? true : false;
+
+console.log(result1);
+
+let Capital1 = "apple";
+let Capital2 = "Apple";
+
+let result2 = Capital1 == Capital2 ? true : false;
+
+console.log(result2);
+
+
+let CheckNum1 = 7;
+let CheckNum2 = 5;
+
+let result3 = CheckNum1 >= CheckNum2 ? true : false;
+
+console.log(result3);
+
+
+let OR1 = 12;
+let OR2 = 22;
+
+
+let result4 = OR1 > OR2 || OR1<=8? true : false;
+
+console.log(result4);
+
+let And1 = 7;
+let And2 = 5;
+
+
+let result5 = And1 > And2 && And1<=8? true : false;
+
+console.log(result5);
+
+
+let List1 = ["Mannan"];
+
+let result6 = List1 ==null? true : false;
+
+
+
+console.log(result6);
+
+let List2 = [];
+let result7 = List2.length === 0 ? true : false;
+console.log(result7);
