2acbfb7c35fd215ab99bcf37066a70e11ddf8e82
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:21:11 +0500

    task22

---
+++ task22.js
@@ -0,0 +1,5 @@
+// Do one mistake in the last index
+// let cars = ["Tesla","Honda Civic" , "Sportage" , 2@];
+// After correct the error
+var cars = ["Tesla", "Honda Civic", "Sportage", "2@"];
+console.log(cars);
---
+++ task22.ts
@@ -0,0 +1,8 @@
+// Do one mistake in the last index
+
+// let cars = ["Tesla","Honda Civic" , "Sportage" , 2@];
+
+// After correct the error
+let cars = ["Tesla","Honda Civic" , "Sportage" , "2@"];
+
+console.log(cars);
