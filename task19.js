340a14c9a8d5a930ff84517f8b581070e4f7ebc7
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-02 14:19:49 +0500

    task19

---
+++ task19.js
@@ -0,0 +1,9 @@
+var List = [
+    "Ashir",
+    "M.Shayan",
+    "Hamza",
+    "Faraz",
+    "Siraj",
+    "Nouman"
+];
+console.log("I invited", List.length, "Perople's For the dinner");
---
+++ task19.ts
@@ -0,0 +1,12 @@
+
+
+let List = [
+    "Ashir",
+    "M.Shayan",
+    "Hamza",
+    "Faraz",
+    "Siraj",
+    "Nouman"
+  ];
+
+  console.log("I invited",List.length, "Perople's For the dinner");
