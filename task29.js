a88c49d7301166caadc0e32772e38659d6018e5d
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-17 17:03:31 +0500

    task 29

---
+++ task29.js
@@ -0,0 +1,24 @@
+// Array of favorite fruits
+var favorite_fruits = ["banana", "apple", "strawberry"];
+// Check if each fruit is in the array
+if (favorite_fruits.indexOf("banana") !== -1) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.indexOf("apple") !== -1) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.indexOf("strawberry") !== -1) {
+    console.log("You really like strawberries!");
+}
+if (favorite_fruits.indexOf("orange") !== -1) {
+    console.log("You really like oranges!");
+}
+else {
+    console.log("You don't have oranges in your favorite fruits.");
+}
+if (favorite_fruits.indexOf("kiwi") !== -1) {
+    console.log("You really like kiwis!");
+}
+else {
+    console.log("You don't have kiwis in your favorite fruits.");
+}
---
+++ task29.ts
@@ -0,0 +1,27 @@
+
+let favorite_fruits: string[] = ["banana", "apple", "strawberry"];
+
+
+if (favorite_fruits.indexOf("banana") !== -1) {
+    console.log("You really like bananas!");
+}
+
+if (favorite_fruits.indexOf("apple") !== -1) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.indexOf("strawberry") !== -1) {
+    console.log("You really like strawberries!");
+}
+
+if (favorite_fruits.indexOf("orange") !== -1) {
+    console.log("You really like oranges!");
+} else {
+    console.log("You don't have oranges in your favorite fruits.");
+}
+
+if (favorite_fruits.indexOf("kiwi") !== -1) {
+    console.log("You really like kiwis!");
+} else {
+    console.log("You don't have kiwis in your favorite fruits.");
+}
