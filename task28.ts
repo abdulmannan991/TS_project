743c3bada4d79a4d77bb872d94f5ca1dd89ba286
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-17 17:02:49 +0500

    Task 28

---
+++ task28.ts
@@ -0,0 +1,24 @@
+let age = 23;
+
+if (age<2) {
+    console.log("The person is baby");
+} 
+else if(age>=2 && age<4){
+    console.log("The person is toddler");
+
+}
+
+else if(age>=4 && age<13){
+    console.log("The person is Kid");
+
+}
+else if(age>=13 && age<20){
+    console.log("The person is teenager");
+}
+else if(age>=20 && age<65){
+    console.log("The person is Adult");
+
+}
+else {
+    console.log("The person is elder");
+}
