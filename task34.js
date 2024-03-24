993724e5970da43fce6fe1334803d801b4543711
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-24 17:24:52 +0500

    task34

---
+++ task34.js
@@ -0,0 +1,10 @@
+var pizzas = ["Pepperoni", "Margherita", "Supreme"];
+console.log("My favorite pizzas:");
+for (var i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+}
+console.log("\nStatements about my favorite pizzas:");
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like " + pizzas[i] + " pizza.");
+}
+console.log("\nI really love pizza!");
---
+++ task34.ts
@@ -0,0 +1,14 @@
+const pizzas = ["Pepperoni", "Margherita", "Supreme"];
+
+console.log("My favorite pizzas:");
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+}
+
+console.log("\nStatements about my favorite pizzas:");
+for (let i = 0; i < pizzas.length; i++) {
+    console.log("I like " + pizzas[i] + " pizza.");
+}
+
+
+console.log("\nI really love pizza!");
