5665c31b7d9b33ac301fa78cbee8d5dcb65792a1
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-24 17:26:05 +0500

    task36

---
+++ task36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("Shirt size: ".concat(size));
+    console.log("Message: ".concat(message));
+}
+make_shirt("Large", "Hello, World!");
---
+++ task36.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`Shirt size: ${size}`);
+    console.log(`Message: ${message}`);
+}
+
+make_shirt("Large", "Hello, World!");
