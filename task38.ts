70c6a2fad97ad7a1f03ec50cdccbe8e2ae050ad0
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-24 17:27:06 +0500

    task38

---
+++ task38.ts
@@ -0,0 +1,7 @@
+function describe_city(city: string, country: string = "Unknown"): void {
+    console.log(`${city} is in ${country}.`);
+}
+
+describe_city("Karachi", "Pakistan");
+describe_city("Tokyo", "Japan");
+describe_city("Paris");
